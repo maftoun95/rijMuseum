6b761d4c8840d0f2fe545bc309bcd26e5a26ad0a
Saman M <maftoun95@gmail.com>
2017-03-27 10:44:33 -0700

    setup commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    bp = require('body-parser'),
+    path = require('path'),
+    session = require('express-session')
+    PORT = 8000;
+
+app.use(express.static(path.join(__dirname,'/client')))
+app.use(express.static(path.join(__dirname,'/node_modules')))
+app.use(bp.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// require('./server/config/mongoose.js');
+
+// require('./server/config/routes.js')(app);
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`)
+})
