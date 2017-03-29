71baa6ab39e98ed9cc8b92e0325cf0f5f1f7c085
Davis Herbig (Neon) <davis.herbig@gmail.com>
2017-03-29 16:03:31 -0700

    Added  finished api calls

--- client/controllers/galleryController.js
+++ client/controllers/galleryController.js
@@ -2,26 +2,26 @@
 
 rijMuse.controller('galleryController', ['$http', '$routeParams', 'canvasFactory', '$scope', function($http, $routeParams, canvasFactory, $scope){
     console.log("In galleryController")
-    var galleryPrefix = 'https://www.rijksmuseum.nl/api/pages/en/rijksstudio/'
+    var galleryPrefix = 'https://www.rijksmuseum.nl/api/pages/en/'
     var keyJson = '?key=xvBxNCX5&format=json&imgonly=true';
     $scope.theGallery = {};
     // List of all availible galleries.  To add a new gallery, take the URL of the page and copy everything from 
     
     // The URLs for for calls to the API are urlPrefix + 'optionalObjectNumber' + keyJson + 'searchTerms'
     var galleryIndex = {
-        "rembrandt": "artists/rembrandt-harmensz-van-rijn",
-        "vanGogh": "artists/vincent-van-gogh",
-        "vermeer": "artists/johannes-vermeer",
-        "paper": "works-of-art/masterpieces-on-paper"
+        "rembrandt": "rijksstudio/artists/rembrandt-harmensz-van-rijn",
+        "vanGogh": "rijksstudio/artists/vincent-van-gogh",
+        "vermeer": "rijksstudio/artists/johannes-vermeer",
+        "paper": "rijksstudio/works-of-art/masterpieces-on-paper"
     }
 
 
     var getGallery = function(galleryName, page = 1){
         if(galleryName === 'undefined'){
-            galleryName = 'paper';
+            galleryName = 'vermeer';
         }
         var pageData = '&p='+page+'&ps=16'
-        var queryURL = galleryPrefix + galleryIndex[galleryName] + keyJson;
+        var queryURL = galleryPrefix + galleryIndex[galleryName] + keyJson + pageData;
         console.log("Query = " + queryURL);
         $http({
             method: 'GET',
@@ -35,19 +35,24 @@ rijMuse.controller('galleryController', ['$http', '$routeParams', 'canvasFactory
             newGallery.artList = jsonData.data.contentPage.artObjectSet;
             newGallery.text = jsonData.data.contentPage.body.markdown;
             newGallery.splash = jsonData.data.contentPage.headerImage;
+            var temp;
             for(var i = 0; i< newGallery.artList.length; i++){
-                var promise = new Promise(function(resolve, reject) {
-                    var temp = canvasFactory(newGallery.artList[i]);
-                }).then(function(response){
-                    newGallery.artList[i] = temp;
-                    console.log("That was image " + (i+1))
-                }
+
+                !function outer(i){
+                    canvasFactory(newGallery.artList[i],function(newCanvas){
+                        if(!newCanvas.url){
+                            newGallery.artList[i] = "()()()()()()()()()()()()()()";
+                        }else{
+                            newGallery.artList[i] = newCanvas;
+                        }
+                        console.log("Image " + (i+1) + ": "+ JSON.stringify(newGallery.artList[i]));
+                    });
+                }(i)
 
 
-                )
             }
+            console.log("TEH BROWSER SEEED THE DATAS AZZZZ ***************************************" + JSON.stringify(newGallery))
             $scope.theGallery = newGallery;
-            console.log(JSON.stringify(newGallery))
             return newGallery;
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
@@ -58,3 +63,4 @@ rijMuse.controller('galleryController', ['$http', '$routeParams', 'canvasFactory
     console.log(getGallery('paper'));
 
 }])
+
--- client/factories/canvasFactory.js
+++ client/factories/canvasFactory.js
@@ -3,7 +3,7 @@
 rijMuse.factory('canvasFactory', [
     '$http', 
     function canvasFactory($http) {
-        return function(canvasID){
+        return function(canvasID, callback){
             var newCanvas = {};
             var prefix = 'https://www.rijksmuseum.nl/api/en/collection/';
             var suffix = '?key=xvBxNCX5&format=json&';
@@ -18,12 +18,12 @@ rijMuse.factory('canvasFactory', [
                 console.log("Canvas JSON Data: " + JSON.stringify(jsonData))
                 newCanvas.id = jsonData.data.artObject.id;
                 // Dimension data should be in cm. If you have trouble rendering, check dimensions[i].unit.
-                if(jsonData.data.artObject.dimensions){
-                    newCanvas.height = jsonData.data.artObject.dimensions[0].value;
-                    newCanvas.width =jsonData.data.artObject.dimensions[1].value;
-                }else{
+                if(jsonData.data.artObject.webImage != null){
                     newCanvas.height = jsonData.data.artObject.webImage.height;
                     newCanvas.width =jsonData.data.artObject.webImage.width;
+                }else{
+                    newCanvas.height = jsonData.data.artObject.dimensions[0].value;
+                    newCanvas.width =jsonData.data.artObject.dimensions[1].value;
                 }
                 
                 newCanvas.url = jsonData.data.artObject.webImage.url;
@@ -39,10 +39,15 @@ rijMuse.factory('canvasFactory', [
                 console.log('New Canvas: ' + JSON.stringify(newCanvas))
                 console.log('-------------------------------------------------------------');
                 // Returns the canvas object.
+                callback(newCanvas);
                 return newCanvas;
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
+                console.log('-------------------------------------------------------------');
+                console.log('Error Response: ' + JSON.stringify(response))
+                console.log('-------------------------------------------------------------');
+                
                 return response;
             });
         }
