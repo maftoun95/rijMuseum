fbf6ed2438959c63bf6020ca0d77c1d65f542238
Saman M <maftoun95@gmail.com>
2017-03-29 18:40:03 -0700

    linking scripts

--- client/script.js
+++ client/script.js
@@ -1,25 +1,27 @@
+var controllaaaaaaa = require('./controllers/galleryController');
+
 var mainDoor = document.querySelector('#mainDoor');
 var rembrandt = document.querySelector('#rembrandt');
 var vanGoh = document.querySelector('#vanGoh');
 var vermeer = document.querySelector('#vermeer');
 var paper = document.querySelector('#paper');
-
-var c1 = document.querySelector('#1');
-var c2 = document.querySelector('#2');
-var c3 = document.querySelector('#3');
-var c4 = document.querySelector('#4');
-var c5 = document.querySelector('#5');
-var c6 = document.querySelector('#6');
-var c7 = document.querySelector('#7');
-var c8 = document.querySelector('#8');
-var c9 = document.querySelector('#9');
-var c10 = document.querySelector('#10');
-var c11 = document.querySelector('#11');
-var c12 = document.querySelector('#12');
-var c13 = document.querySelector('#13');
-var c14 = document.querySelector('#14');
-var c15 = document.querySelector('#15');
-var c16 = document.querySelector('#16');
+console.log("~~~~~~~~~~~~~~~~~",rembrandt)
+var c1 = document.getElementById('1');
+var c2 = document.getElementById('2');
+var c3 = document.getElementById('3');
+var c4 = document.getElementById('4');
+var c5 = document.getElementById('5');
+var c6 = document.getElementById('6');
+var c7 = document.getElementById('7');
+var c8 = document.getElementById('8');
+var c9 = document.getElementById('9');
+var c10 = document.getElementById('10');
+var c11 = document.getElementById('11');
+var c12 = document.getElementById('12');
+var c13 = document.getElementById('13');
+var c14 = document.getElementById('14');
+var c15 = document.getElementById('15');
+var c16 = document.getElementById('16');
 
 var canvases = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16];
 
@@ -29,13 +31,7 @@ mainDoor.addEventListener('click', function(){
     console.log('yay main button')
 })
 rembrandt.addEventListener('click', function(){
-    var set = galleryController.getGallery('rembrandt', 1, function(){
-        // for(var i = 0; i < canvases.length; i++){
-        //     canvases[i].setAttribute()
-        // }
-        console.log($scope.theGallery);
-    });
-
+    controllaaaaaaa.getGallery("rembrandt")
 })
 vanGoh.addEventListener('click', function(){
     console.log('fire controller call to API: vanGoh')
